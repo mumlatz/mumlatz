314da9c7cacec8bbbe02ec1acd6e4cd4715c9137
Uri Klar <uriklar@gmail.com>
2022-10-30 19:59:35 +0200

    get main categories text

--- packages/scraper/index.ts
+++ packages/scraper/index.ts
@@ -1,8 +1,18 @@
 import cheerio from "cheerio";
-
+import axios from "axios";
 export async function wallaShops() {
-  const response = await cheerio.load("https://www.wallashops.co.il");
-  console.log(response);
+  const { data } = await axios.get("https://www.wallashops.co.il");
+  const $ = cheerio.load(data);
+
+  // extract main nav menu items
+  const $mainCategories = $(".first-level-list .first-level-list-item > a");
+  // get inner text from mainNavItems and remove all \n and \t
+  const mainCategoriesText = $mainCategories
+    .map((i, el) => $(el).text())
+    .get()
+    .map((item) => item.replace(/[\n\t]/g, ""));
+
+  console.log(mainCategoriesText);
 }
 
 wallaShops();
